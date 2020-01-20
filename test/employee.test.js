e3d8bf59f623cc8ed10790528a244907a757058b
deebs <hereisdanbrown@gmail.com>
2020-01-20 16:18:15 -0700

    Added test functionality. Haven't passed all the tests yet.

--- test/employee.test.js
+++ test/employee.test.js
@@ -1,46 +1,46 @@
 const Employee = require("../lib/Employee");
-
+// When you create an instance of employee it is an object
 test("Can instantiate Employee instance", () => {
   const e = new Employee();
   expect(typeof(e)).toBe("object");
 });
-
+// Can set name via constructor arguments
 test("Can set name via constructor arguments", () => {
   const name = "Alice";
   const e = new Employee(name);
   expect(e.name).toBe(name);
 });
-
+// Can set id via constructor arguments
 test("Can set id via constructor argument", () => {
   const testValue = 100;
   const e = new Employee("Foo", testValue);
   expect(e.id).toBe(testValue);
 });
-
+// Can set email via constructor arguments
 test("Can set email via constructor argument", () => {
   const testValue = "test@test.com";
   const e = new Employee("Foo", 1, testValue);
   expect(e.email).toBe(testValue);
 });
-
+// Can return the game using getName method
 test("Can get name via getName()", () => {
   const testValue = "Alice";
   const e = new Employee(testValue);
   expect(e.getName()).toBe(testValue);
 });
-
+// Can return id via getID method
 test("Can get id via getId()", () => {
   const testValue = 100;
   const e = new Employee("Foo", testValue);
   expect(e.getId()).toBe(testValue);
 });
-
+// Can return email via getEmail()
 test("Can get email via getEmail()", () => {
   const testValue = "test@test.com";
   const e = new Employee("Foo", 1, testValue);
   expect(e.getEmail()).toBe(testValue);
 });
-
+// Can return "Employee" running getRole()
 test("getRole() should return \"Employee\"", () => {
   const testValue = "Employee";
   const e = new Employee("Alice", 1, "test@test.com");
